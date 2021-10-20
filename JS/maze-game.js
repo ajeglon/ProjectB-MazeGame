81224a850da734446b79da49bcb1beb7f9606895
ajeglon <61421392+ajeglon@users.noreply.github.com>
2021-10-20 14:36:01 +0100

    Added on screen buttons

--- JS/maze-game.js
+++ JS/maze-game.js
@@ -1,8 +1,8 @@
 let levels = [];
-levels['0'] = {
+levels[0] = {
   map: [
     [1, 1, 0, 0, 1, 0, 1, 0, 1, 0],
-    [1, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+    [1, 1, 0, 0, 0, 0, 1, 0, 1, 0],
     [1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
     [0, 0, 1, 1, 0, 0, 1, 0, 1, 0],
     [0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
@@ -18,11 +18,13 @@ levels['0'] = {
     y: 4
   },
   exit: {
-    x: 0,
-    y: 5
+    x: 4,
+    y: 1
   },
   theme: 'Easy',
   levelDimension: 48,
+  playerHealth: 100,
+  goldValues: [10, 20, 30, 40, 50]
 };
 
 function mazeGame(id, level) {
@@ -33,6 +35,8 @@ function mazeGame(id, level) {
   this.theme = level.theme;
   this.player = { ...level.player };
   this.exit = { ...level.exit };
+  this.playerHealth = level.playerHealth;
+  this.goldValues = level.goldValues;
 }
 
 mazeGame.prototype.populateMap = function () {
@@ -58,7 +62,6 @@ mazeGame.prototype.createSprite = function (x, y, type) {
 }
 
 mazeGame.prototype.sizeUp = function () {
-
   let map = this.element.querySelector('.maze-map');
   map.style.height = this.map.length * this.tileDimension + 'px';
   map.style.width = this.map[0].length * this.tileDimension + 'px';
@@ -75,6 +78,22 @@ mazeGame.prototype.placeSprite = function (type) {
   return sprite;
 }
 
+mazeGame.prototype.generateGold = function () {
+  // let addGold = document.getElementsByClassName('floor');
+
+  // addGold.className += '-gold';
+
+  // if (addGold = 'floor-gold') {
+  //   console.log('Gold')
+  // } else {
+  //   console.log('No Gold')
+  // }
+
+
+
+}
+
+
 mazeGame.prototype.keyboardListener = function () {
   document.addEventListener('keydown', event => {
     this.movePlayer(event);
@@ -82,85 +101,101 @@ mazeGame.prototype.keyboardListener = function () {
   });
 }
 
-mazeGame.prototype.movePlayer = function (event) {
-
+mazeGame.prototype.movePlayerArrows = function (event) {
   event.preventDefault();
-
   if (event.keyCode < 37 || event.keyCode > 40) {
     return;
   }
   switch (event.keyCode) {
-
     case 37:
       this.moveLeft();
       break;
-
     case 38:
       this.moveUp();
       break;
-
     case 39:
       this.moveRight();
       break;
-
     case 40:
       this.moveDown();
       break;
   }
 }
-mazeGame.prototype.keyboardListener = function () {
-  document.addEventListener('keydown', event => {
-    this.movePlayer(event);
-  });
+
+mazeGame.prototype.movePlayerWASD = function (event) {
+  event.preventDefault();
+  if (event.keyCode < 65 || event.keyCode > 87) {
+    return;
+  }
+  switch (event.keyCode) {
+    case 65:
+      this.moveLeft();
+      break;
+    case 87:
+      this.moveUp();
+      break;
+    case 68:
+      this.moveRight();
+      break;
+    case 83:
+      this.moveDown();
+      break;
+  }
 }
 
 mazeGame.prototype.moveLeft = function (sprite) {
   if (this.player.x == 0) {
+    this.collide();
     return;
   }
   let nextTile = this.map[this.player.y][this.player.x - 1];
   if (nextTile == 1) {
+    this.collide();
     return;
   }
   this.player.x -= 1;
-
   this.updateHoriz(sprite);
 }
+
 mazeGame.prototype.moveUp = function () {
   if (this.player.y == 0) {
+    this.collide();
     return;
   }
   let nextTile = this.map[this.player.y - 1][this.player.x];
   if (nextTile == 1) {
+    this.collide();
     return;
   }
   this.player.y -= 1;
-
   this.updateVert();
 }
+
 mazeGame.prototype.moveRight = function (sprite) {
   if (this.player.x == this.map[this.player.y].length - 1) {
+    this.collide();
     return;
   }
   let nextTile = this.map[this.player.y][this.player.x + 1];
-
   if (nextTile == 1) {
+    this.collide();
     return;
   }
   this.player.x += 1;
-
   this.updateHoriz(sprite);
 }
+
 mazeGame.prototype.moveDown = function () {
   if (this.player.y == this.map.length - 1) {
+    this.collide();
     return;
   }
   let nextTile = this.map[this.player.y + 1][this.player.x];
   if (nextTile == 1) {
+    this.collide();
     return;
   }
   this.player.y += 1;
-
   this.updateVert();
 }
 
@@ -172,33 +207,69 @@ mazeGame.prototype.updateHoriz = function (sprite) {
   this.player.element.style.left = this.player.x * this.tileDimension + 'px';
 };
 
-
 mazeGame.prototype.checkGoal = function () {
-
-  let mazeGameContainer = document.querySelector('maze-game-container');
-
+  let mapAndControls = document.getElementById('maze-map-and-controls');
   if (this.player.y == this.exit.y &&
     this.player.x == this.exit.x) {
-    mazeGameContainer.className = 'exit-message';
+    mapAndControls.className = 'success';
   }
   else {
-    mazeGameContainer.className = '';
+    mapAndControls.className = '';
   }
-
 }
 
+mazeGame.prototype.keyboardListener = function () {
+  document.addEventListener('keydown', event => {
+    this.movePlayerArrows(event);
+    this.movePlayerWASD(event);
+    this.checkGoal();
+  });
+}
+
+mazeGame.prototype.buttonListeners = function (instrux_msg, goal_msg) {
+  let up = document.getElementById('up');
+  let left = document.getElementById('left');
+  let down = document.getElementById('down')
+  let right = document.getElementById('right');
+  let obj = this;
+  up.addEventListener('mousedown', function () {
+    obj.moveUp();
+    obj.checkGoal(instrux_msg, goal_msg);
+  });
+  down.addEventListener('mousedown', function () {
+    obj.moveDown();
+    obj.checkGoal(instrux_msg, goal_msg);
+  });
+  left.addEventListener('mousedown', function () {
+    obj.moveLeft();
+    obj.checkGoal(instrux_msg, goal_msg);
+  });
+  right.addEventListener('mousedown', function () {
+    obj.moveRight();
+    obj.checkGoal(instrux_msg, goal_msg);
+  });
+}
+
+mazeGame.prototype.collide = function () {
+  this.player.element.className += ' collide';
+  let obj = this;
+  window.setTimeout(function () {
+    obj.player.element.className = 'player';
+  }, 200);
+  return 0;
+};
 
 function init() {
   let myGame = new mazeGame('maze-game-container-1', levels[0]);
-
   myGame.populateMap();
   myGame.sizeUp();
-
+  // myGame.placeSprite('maze-gold')
   myGame.placeSprite('exit');
   let playerSprite = myGame.placeSprite('player');
   myGame.player.element = playerSprite;
-
   myGame.keyboardListener();
+  myGame.buttonListeners();
+  myGame.generateGold();
 }
 init();
 
